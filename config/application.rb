2cbfb8ca8cdb90a3755b462312c33e5d6fb888fe
Mariko <maripiyok@gmail.com>
2015-07-23 23:27:57 +0900

    RSpec初期設定
    ./bin/rails g rspec:install
    bundle binstubs rspec-core

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,22 @@ module SimpleQa
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # RSpec
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.template_engine :haml
+      g.test_frameword :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+      g.template_engine :haml
+    end
   end
 end
